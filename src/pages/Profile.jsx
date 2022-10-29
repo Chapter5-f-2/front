eecf82d8621c595bf26c9335830de51211338d00
hansejun <tpwnszja2@gmail.com>
2022-10-30 03:55:40 +0900

    계획

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Footer from "../components/footer/Footer";
 import DetailHeader from "../components/header/DetailHeader";
